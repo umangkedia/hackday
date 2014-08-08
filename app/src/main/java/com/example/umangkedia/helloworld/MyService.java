7e38840feabbc8b3106b3e5e5ca64d37e1e45a2f
Umang Kedia <umang.kedia@umang-kedia.local>
2014-08-08 11:09:37 +0530

    location update

--- app/src/main/java/com/example/umangkedia/helloworld/MyService.java
+++ app/src/main/java/com/example/umangkedia/helloworld/MyService.java
@@ -15,10 +15,14 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.util.Log;
+import android.widget.Toast;
+
+import javax.crypto.spec.GCMParameterSpec;
 
 public class MyService extends Service {
 
@@ -27,9 +31,12 @@ public class MyService extends Service {
     private MyThread mythread;
     public boolean isRunning = false;
     NotificationManager notificationManager;
-    private LocationManager locationManager;
-    Location location;
-    public Handler handler;
+    Location curLocation;
+    public static boolean isService = true;
+
+    private LocationManager locMan;
+    private Boolean locationChanged;
+    private Handler handler = new Handler();
 
     double latitude = 0;
     double longitude = 0;
@@ -45,16 +52,19 @@ public class MyService extends Service {
         Log.d(TAG, "onCreate");
         notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
         showNotification();
-
-        handler = new Handler() {
-            @Override
-            public void handleMessage(Message msg) {
-                location = getLocation();
-            }
-        };
-
         mythread  = new MyThread();
+
+        curLocation = getBestLocation();
+
+        if (curLocation == null)
+            Toast.makeText(getBaseContext(), "Unable to get your location", Toast.LENGTH_SHORT).show();
+        else{
+            //Toast.makeText(getBaseContext(), curLocation.toString(), Toast.LENGTH_LONG).show();
+        }
+
+        isService =  true;
     }
+
 
     @Override
     public synchronized void onDestroy() {
@@ -68,6 +78,8 @@ public class MyService extends Service {
 
     @Override
     public synchronized int onStartCommand(Intent intent, int flags, int startId) {
+        handler.postDelayed(GpsFinder, 1);
+
         Log.d(TAG, "onStart");
         if(!isRunning){
             mythread.start();
@@ -76,35 +88,60 @@ public class MyService extends Service {
         return super.onStartCommand(intent, flags, startId);
     }
 
-    public Location getLocation() {
-        try {
-            locListener locList = new locListener();
-            locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
-            // getting GPS status
-            boolean isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-            // if GPS Enabled get lat/long using GPS Services
-            if (isGPSEnabled) {
-                if (location == null) {
-                    locationManager.requestLocationUpdates(
-                            LocationManager.GPS_PROVIDER,
-                            40000,
-                            MIN_DISTANCE_CHANGE_FOR_UPDATES, locList);
+    private Location getBestLocation() {
+        Location gpslocation = null;
+        Location networkLocation = null;
 
-                    if (locationManager != null) {
-                        location = locationManager
-                                .getLastKnownLocation (LocationManager.GPS_PROVIDER);
-                        if (location != null) {
-                            latitude = location.getLatitude();
-                            longitude = location.getLongitude();
-                        }
-                    }
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+        if(locMan==null){
+            locMan = (LocationManager) getApplicationContext() .getSystemService(getApplicationContext().LOCATION_SERVICE);
         }
-        return location;
+        try {
+            if(locMan.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+                locMan.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000, 1, gpsListener);// here you can set the 2nd argument time interval also that after how much time it will get the gps location
+                gpslocation = locMan.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+            }
+            if(locMan.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+                locMan.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,1000, 1, gpsListener);
+                networkLocation = locMan.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            }
+        } catch (IllegalArgumentException e) {
+            //Log.e(ErrorCode.ILLEGALARGUMENTERROR, e.toString());
+            Log.e("error", e.toString());
+        }
+        if(gpslocation==null && networkLocation==null)
+            return null;
+
+        if(gpslocation!=null && networkLocation!=null){
+            if(gpslocation.getTime() < networkLocation.getTime()){
+                gpslocation = null;
+                return networkLocation;
+            }else{
+                networkLocation = null;
+                return gpslocation;
+            }
+        }
+        if (gpslocation == null) {
+            return networkLocation;
+        }
+        if (networkLocation == null) {
+            return gpslocation;
+        }
+        return null;
     }
+
+    public Runnable GpsFinder = new Runnable(){
+        public void run(){
+
+            Location tempLoc = getBestLocation();
+            Log.d(TAG, String.valueOf(tempLoc.getLatitude() + String.valueOf(tempLoc.getLongitude())));
+
+            if(tempLoc!=null)
+                curLocation = tempLoc;
+
+            handler.postDelayed(GpsFinder,40000);// register again to start after 40 seconds...
+        }
+    };
 
     public void readWebPage(){
         HttpClient client = new DefaultHttpClient();
@@ -163,4 +200,38 @@ public class MyService extends Service {
 
         notificationManager.notify(0, n);
     }
+
+    LocationListener gpsListener = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            if (curLocation == null) {
+                curLocation = location;
+                locationChanged = true;
+            }else if (curLocation.getLatitude() == location.getLatitude() && curLocation.getLongitude() == location.getLongitude()){
+                locationChanged = false;
+                return;
+            }else
+                locationChanged = true;
+
+            curLocation = location;
+
+            if (locationChanged)
+                locMan.removeUpdates(gpsListener);
+
+        }
+        public void onProviderDisabled(String provider) {
+        }
+
+        public void onProviderEnabled(String provider) {
+        }
+
+        public void onStatusChanged(String provider, int status,Bundle extras) {
+            if (status == 0)// UnAvailable
+            {
+            } else if (status == 1)// Trying to Connect
+            {
+            } else if (status == 2) {// Available
+            }
+        }
+
+    };
 }
